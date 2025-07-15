9f242fd4ab38678eab6e856041d5d7a7d8027b58
FranciscoMessina <francisco@messina.ws>
2025-07-14 22:37:14 -0300

    trying something

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,6 +4,20 @@ import Header from "../components/Header";
 import { Toaster } from "../components/ui/sonner";
 
 export const Route = createRootRoute({
+	head: () => ({
+		title: "Courtpadel",
+		meta: [
+			{
+				name: "description",
+				content: "Courtpadel",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1.0, interactive-widget=resizes-content",
+			},
+
+		],
+	}),
 	component: () => (
 		<>
 			<Header />
